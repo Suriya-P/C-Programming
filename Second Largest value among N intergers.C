4d35b42798654a75d6d670790047073c4d6fff58
Suriya-P <105926065+Suriya-P@users.noreply.github.com>
2023-02-15 22:33:17 +0530

    Update Second Largest value among N intergers.C

--- Second Largest value among N intergers.C
+++ Second Largest value among N intergers.C
@@ -7,19 +7,20 @@
 #include <stdio.h>
 
 int main() {
-    int n,i,j,k=0,s[1000],a[1000];
+    int n,i,j,t,a[1000];
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
     for(i=0;i<n;i++){
-        for(j=i;j<n;j++){
+        for(j=i+1;j<n;j++){
             if(a[i]>a[j]){
-                s[k]=a[i];
-                k++;
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
             }
         }
     }
-    printf("%d %d",s[k-2],s[k-1]);
+    printf("%d",a[n-2]);
     return 0;
 }
